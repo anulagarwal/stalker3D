68973d296ee5c967971857aadd1715a3c5f6850e
Anul Agarwal <anulagarwal12@gmail.com>
2021-12-15 16:45:16 +0530

    Project setup

---
+++ Assets/Scripts/Support/EnumsManager.cs
@@ -0,0 +1,44 @@
+public enum PlayerAnimationState
+{
+    Idle,
+    Run,
+    Victory,
+    Defeat
+};
+
+public enum UIPanelState
+{
+    MainMenu,
+    Gameplay,
+    GameWin,
+    GameLose
+};
+
+public enum GameOverState
+{
+    Victory,
+    Defeat,
+    None
+};
+public enum GameState
+{
+    Main,
+    InGame,
+    Win,
+    Lose
+}
+public enum CatState
+{
+    WalkSleep,
+    Sleep,
+    Irritated,
+    Awake
+}
+
+public enum Sound
+{
+    Purr,
+    Meow,
+    Scream,
+    Hiss
+}
